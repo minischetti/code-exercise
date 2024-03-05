2429776c5e52ffb8cbecee0ff16d0160af5e991d
Dominic Minischetti III <domminischetti@gmail.com>
2024-03-05 18:58:24 +0000

    Add solution with list and table

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  useEffect(() => {}, []);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.products);
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1>Abercrombie & Fitch</h1>
-      <h2>Products</h2>
+      <h2>Products - {products.length}</h2>
+      <ul>
+        {products &&
+          products.map((product) => (
+            <li key={product.id}>
+              {product.name} - ${product.price}
+            </li>
+          ))}
+      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products &&
+            products.map((product) => (
+              <tr key={product.id}>
+                <td>{product.name}</td>
+                <td>${product.price}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/server.js
+++ src/server.js
@@ -7,9 +7,21 @@ import Cors from "@koa/cors";
 const app = new Koa();
 const router = new Router();
 
+const generateRandomProducts = () => {
+  const products = [];
+  for (let i = 0; i < 30; i++) {
+    products.push({
+      id: i,
+      name: `Product ${i}`,
+      price: Math.floor(Math.random() * 1000),
+    });
+  }
+  return products;
+};
+
 router.get("/", async (ctx) => {
   ctx.body = {
-    products: [],
+    products: generateRandomProducts(),
   };
 });
 
